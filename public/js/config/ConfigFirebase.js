95640a3f97b32ccac762ce4892caafe862d39c40
EdwardMarinao <69734232+EdwardMarinao@users.noreply.github.com>
2021-09-08 01:30:12 -0300

    Add files via upload

---
+++ public/js/config/ConfigFirebase.js
@@ -0,0 +1,9 @@
+const varconfig = {
+    apiKey: "AIzaSyAbGe2XTDxXGg8yfei2ztx9a-SjY0RHIuk",
+    authDomain: "overmind-d1b99.firebaseapp.com",
+    projectId: "overmind-d1b99",
+    storageBucket: "overmind-d1b99.appspot.com",
+    messagingSenderId: "243879402726",
+    appId: "1:243879402726:web:59906ffc724243cc372f27",
+    measurementId: "G-EGFCW1BX9H"
+};
